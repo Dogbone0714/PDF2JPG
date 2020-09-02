1c34e3b446ae87fe7e64e1b9e1d21583177d827f
Dogbone0714 <jameskang0714@gmail.com>
2020-09-03 00:27:02 +0800

    Update pdf2jpg.py

--- pdf2jpg.py
+++ pdf2jpg.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tempfile
 from tkinter import filedialog
 from tkinter import messagebox
 from pdf2image import convert_from_path, convert_from_bytes
@@ -11,3 +12,12 @@ canvas1.pack()
 label1 = tk.Label(root, text='PDF To JPG Tool', bg='lightsteelblue2')
 label1.config(font=('helvetica', 20))
 canvas1.create_window(150, 50, window=label1)
+
+
+
+
+
+
+
+
+root.mainloop()

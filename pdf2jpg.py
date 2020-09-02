c0e87e7d004c49eb5939e27542090fb05a916d6a
Dogbone0714 <jameskang0714@gmail.com>
2020-09-03 00:33:47 +0800

    Update pdf2jpg.py

--- pdf2jpg.py
+++ pdf2jpg.py
@@ -4,7 +4,7 @@ from tkinter import filedialog
 from tkinter import messagebox
 from pdf2image import convert_from_path, convert_from_bytes
 
-root= tk.Tk()
+root = tk.Tk()
 root.title("PDF To JPG By HHK")
 canvas1 = tk.Canvas(root, width=300, height=250, bg='lightsteelblue2', relief='raised')
 canvas1.pack()
@@ -14,10 +14,12 @@ label1.config(font=('helvetica', 20))
 canvas1.create_window(150, 50, window=label1)
 
 
+def GetPDF():
+    images = convert_from_bytes(open('/home/belval/example.pdf', 'rb').read())
 
 
-
-
-
+browseButton_PDF = tk.Button(text="      Import   PDF   File     ", command=getPDF, bg='royalblue', fg='white',
+                             font=('helvetica', 12, 'bold'))
+canvas1.create_window(150, 120, window=browseButton_PDF)
 
 root.mainloop()

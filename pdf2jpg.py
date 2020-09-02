3b01ce0a5d8bb3f596cf7a95b3343c0b0b5e590e
Dogbone0714 <jameskang0714@gmail.com>
2020-09-03 00:23:15 +0800

    Update pdf2jpg.py

--- pdf2jpg.py
+++ pdf2jpg.py
@@ -2,3 +2,8 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 from pdf2image import convert_from_path, convert_from_bytes
+
+root= tk.Tk()
+root.title("PDF To JPG By HHK")
+canvas1 = tk.Canvas(root, width=300, height=250, bg='lightsteelblue2', relief='raised')
+canvas1.pack()

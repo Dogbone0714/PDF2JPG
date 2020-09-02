c5b4a4dbf13496bc1abf6edc8c69f338798fcb74
Dogbone0714 <jameskang0714@gmail.com>
2020-09-03 00:24:10 +0800

    Update pdf2jpg.py

--- pdf2jpg.py
+++ pdf2jpg.py
@@ -7,3 +7,7 @@ root= tk.Tk()
 root.title("PDF To JPG By HHK")
 canvas1 = tk.Canvas(root, width=300, height=250, bg='lightsteelblue2', relief='raised')
 canvas1.pack()
+
+label1 = tk.Label(root, text='PDF To JPG Tool', bg='lightsteelblue2')
+label1.config(font=('helvetica', 20))
+canvas1.create_window(150, 50, window=label1)
